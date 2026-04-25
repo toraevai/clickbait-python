67b448d09e6c376b7d06b65744f38213b9754202
toraevai <toraev_aleksei@mail.ru>
2026-04-25 15:56:53 +0300

    Added file parsing and clickbait processing.

---
+++ main.py
@@ -0,0 +1,47 @@
+import argparse
+from pathlib import Path
+import csv
+from tabulate import tabulate
+
+def parse_file(path, videos):
+    with open(path, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            videos.append(row)
+
+def process_clickbait(videos):
+    filtered_videos = [video for video in videos if float(video["ctr"]) > 15 and int(video["retention_rate"]) < 40]
+    filtered_videos.sort(key=lambda video: video["ctr"], reverse=True)
+    columns_to_show = ["title", "ctr", "retention_rate"]
+    info_to_show = [{column: video[column] for column in columns_to_show} for video in filtered_videos]
+    print(tabulate(info_to_show, headers="keys", tablefmt="grid"))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--files", nargs="*", help="Названия файлов для обработки, например: 'stats1.csv stats2.csv', '*.csv'")
+    parser.add_argument("--report", nargs="*", help="Название выводимого отчета, например: 'clickbait'")
+    args = parser.parse_args()
+    files = args.files
+    
+    videos = []
+    if files:
+        if ('*.csv' in files):
+            for path in Path('.').glob('*.csv'):
+                parse_file(path, videos)
+        else:
+            for file in files:
+                parse_file(file, videos)
+    else:
+        print("Вы не указали файлы, который необходимо обработать")
+    
+    reports = args.report
+    for report in reports:
+        match report:
+            case "clickbait":
+                process_clickbait(videos)
+            case _:
+                print(f"Отчет {report} еще не реализован")
+
+
+if __name__ == "__main__":
+    main()
