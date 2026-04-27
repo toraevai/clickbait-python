03135c8665c4406d973a95ac223c8fdf4de8ab68
toraevai <toraev_aleksei@mail.ru>
2026-04-27 08:14:14 +0300

    Added script description.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 import csv
 from tabulate import tabulate
+import textwrap
 
 def parse_file(path, videos):
     if Path(path).exists():
@@ -14,14 +15,20 @@ def parse_file(path, videos):
         print(f"Файл по пути {path} отсутствует")
 
 def process_clickbait(videos):
-    filtered_videos = [video for video in videos if float(video["ctr"]) > 15 and int(video["retention_rate"]) < 40]
-    filtered_videos.sort(key=lambda video: video["ctr"], reverse=True)
+    filtered_videos = [video for video in videos if float(video["ctr"]) > 15 and float(video["retention_rate"]) < 40]
+    filtered_videos.sort(key=lambda video: float(video["ctr"]), reverse=True)
     columns_to_show = ["title", "ctr", "retention_rate"]
     info_to_show = [{column: video[column] for column in columns_to_show} for video in filtered_videos]
     print(tabulate(info_to_show, headers="keys", tablefmt="grid"))
 
 def main():
-    parser = argparse.ArgumentParser()
+    description = textwrap.dedent("""\
+Утилита для обработки .csv файлов, содержащих инфомрацию о видео с полями: title,ctr,retention_rate,views,likes,avg_watch_time. Выводит отчеты по данным файлам.
+На данный момент реализован только отчет clickbait, который позволяет увидеть список видео, отсортированный по убыванию ctr с характеристиками ctr > 15 и retention_rate < 40""")
+    parser = argparse.ArgumentParser(
+        description = description,
+        formatter_class = argparse.RawDescriptionHelpFormatter
+    )
     parser.add_argument("--files", nargs="*", help="Названия файлов для обработки, например: 'stats1.csv stats2.csv', '*.csv'")
     parser.add_argument("--report", nargs="*", help="Название выводимого отчета, например: 'clickbait'")
     args = parser.parse_args()
